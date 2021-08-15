1a6307a948cbe1fdced5023f0e6cb11386771ca0
rohit-singh7 <rohitsingh4137@gmail.com>
2021-08-15 18:21:21 +0530

    Add existing file

---
+++ restserviceaugApplication/src/main/java/com/example/restserviceaugApplication/RestserviceaugApplication.java
@@ -0,0 +1,13 @@
+package com.example.restserviceaugApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestserviceaugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestserviceaugApplication.class, args);
+	}
+
+}
